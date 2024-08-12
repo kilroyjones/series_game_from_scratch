8f98c0410936145cebb2b2ee5554681c97727eeb
kilroy <tsbach@gmail.com>
2024-08-12 15:21:31 +0800

    updated

--- 4_io_uring_tcp/target/debug/build/io_uring_tcp-876e8eb82dd68ed1/build_script_build-876e8eb82dd68ed1.d
+++
@@ -1,5 +0,0 @@
-/home/kilroy/code/rust/series_game_from_scratch/4_io_uring_tcp/target/debug/build/io_uring_tcp-876e8eb82dd68ed1/build_script_build-876e8eb82dd68ed1: build.rs
-
-/home/kilroy/code/rust/series_game_from_scratch/4_io_uring_tcp/target/debug/build/io_uring_tcp-876e8eb82dd68ed1/build_script_build-876e8eb82dd68ed1.d: build.rs
-
-build.rs:
--- 4_io_uring_tcp/target/debug/deps/io_uring_tcp-cdc5e9e2a914dc2f.d
+++
@@ -1,8 +0,0 @@
-/home/kilroy/code/rust/series_game_from_scratch/4_io_uring_tcp/target/debug/deps/libio_uring_tcp-cdc5e9e2a914dc2f.rmeta: src/main.rs src/io_uring.rs
-
-/home/kilroy/code/rust/series_game_from_scratch/4_io_uring_tcp/target/debug/deps/io_uring_tcp-cdc5e9e2a914dc2f.d: src/main.rs src/io_uring.rs
-
-src/main.rs:
-src/io_uring.rs:
-
-# env-dep:OUT_DIR
